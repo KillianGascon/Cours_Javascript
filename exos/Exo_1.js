ecdd305ff47bd2d8684c26dc26211db7e0de1e71
KillianGascon <killian.gascon@ecoles-epsi.net>
2024-11-22 09:09:05 +0100

    Commmit: remise en ordre

---
+++ exos/Exo_1.js
@@ -0,0 +1,5 @@
+let largeur_rectangle = 10;
+let longueur_rectangle = 20;
+
+let perimetre = 2 * (largeur_rectangle + longueur_rectangle);
+console.log(`Le périmètre du rectangle est de ${perimetre} cm`);
